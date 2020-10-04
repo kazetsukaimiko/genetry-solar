f6f15b758168eb394bc35c34f4ba3f7c0b81f623
Scott Serbousek <airetamstrm@gmail.com>
2020-10-04 14:25:45 -0400

    Initial Commit

---
+++ src/main/java/com/genetrysolar/victor/cdi/producer/LiquibaseProducer.java
@@ -0,0 +1,36 @@
+package com.genetrysolar.victor.cdi.producer;
+
+import liquibase.integration.cdi.CDILiquibaseConfig;
+import liquibase.integration.cdi.annotations.LiquibaseType;
+import liquibase.resource.ClassLoaderResourceAccessor;
+import liquibase.resource.ResourceAccessor;
+
+import javax.annotation.Resource;
+import javax.enterprise.inject.Produces;
+import javax.sql.DataSource;
+import java.sql.SQLException;
+
+public  class  LiquibaseProducer  {
+
+    @Resource
+    private DataSource myDataSource;
+
+    @Produces
+    @LiquibaseType
+    public CDILiquibaseConfig createConfig()  {
+        CDILiquibaseConfig  config  =  new  CDILiquibaseConfig();
+        config.setChangeLog("liquibase/genetry/databaseChangeLog.xml");
+        return  config;
+    }
+
+    @Produces @LiquibaseType
+    public DataSource createDataSource()  throws SQLException {
+        return  myDataSource;
+    }
+
+    @Produces @LiquibaseType
+    public ResourceAccessor create()  {
+        return  new ClassLoaderResourceAccessor(getClass().getClassLoader());
+    }
+
+}
