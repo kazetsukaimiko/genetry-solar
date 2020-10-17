98ea801dc8ff3dca6693943af3156533eafb7f90
Scott Serbousek <airetamstrm@gmail.com>
2020-10-17 19:31:01 -0400

    Refactor web into three modules: api, jpa, and web.

--- src/main/java/com/genetrysolar/inverter/InverterBrand.java
+++
@@ -1,9 +0,0 @@
-package com.genetrysolar.inverter;
-
-/*
- * WiFi boards can talk to two kinds of inverters, after all.
- */
-public enum InverterBrand {
-    POWERJACK,
-    GENETRY_SOLAR
-}
--- src/main/java/com/genetrysolar/inverter/InverterVariant.java
+++
@@ -1,7 +0,0 @@
-package com.genetrysolar.inverter;
-
-// What kind of inverter am I
-public class InverterVariant {
-    private InverterBrand brand;
-    private InverterModel version;
-}
